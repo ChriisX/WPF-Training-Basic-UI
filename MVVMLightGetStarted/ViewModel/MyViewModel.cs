311d5568bf76b0b2820a4da5484206323936cdd1
Christophe Chichmanian <chris.chich@hotmail.fr>
2019-07-19 13:52:23 +0200

    Add project files.

---
+++ MVVMLightGetStarted/ViewModel/MyViewModel.cs
@@ -0,0 +1,33 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMLightGetStarted.ViewModel
+{
+    public class MyViewModel : ViewModelBase
+    {
+        private string _myText = "Hello, World!";
+
+        public string MyText
+        {
+            get
+            {
+                return _myText;
+            }
+            set
+            {
+                if (_myText == value)
+                {
+                    return;
+                }
+
+                _myText = value;
+
+                RaisePropertyChanged("MyText");
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Youtube WPF playlist - First project
+
+This project is the first of the WPF series. It is only about developing a simple UI with Grids and StackPanels.
+
+It can be considered as an introduction to XAML.
